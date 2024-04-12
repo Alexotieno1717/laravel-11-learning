04f32257c1c22827988bd845d28eca5518a1f3b4
Alexotieno1717 <alexotieno900@gmail.com>
2024-04-12 11:11:10 +0300

    [feat]: CRUD (Creating, Updating, and Deleting a Resource)

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -4,7 +4,11 @@
         Job
     </x-slot:heading>
 
-    <h2>{{ $job['title'] }}</h2>
-    <p>pays {{ $job['salary'] }} per year</p>
+    <h2>{{ $job->title }}</h2>
+    <p>pays {{ $job->salary }} per year</p>
+
+    <div class="mt-3">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    </div>
 
 </x-layout>
